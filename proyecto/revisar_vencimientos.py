c37cabd6c72082319dcef631afb142c60081a132
Agustin-Barr <agusbarrios2711@gmail.com>
2025-04-12 17:05:24 -0300

    y

--- proyecto/revisar_vencimientos.py
+++ proyecto/revisar_vencimientos.py
@@ -4,4 +4,3 @@ with open ('productos.json', 'r', encoding='utf-8') as vencimientos:
 
 print("productos cargados: ")
 for producto in productos :
-    print(f"- {producto['nombre']}|Vence : {producto['fecha_vencimiento']}| en {producto['sucursal']} ")
