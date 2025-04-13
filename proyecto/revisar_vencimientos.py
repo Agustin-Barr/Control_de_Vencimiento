51bd6982c435a571e696906dd9993ce8b7136d27
Agustin-Barr <agusbarrios2711@gmail.com>
2025-04-13 09:43:06 -0300

    Update revisar_vencimientos.py

--- proyecto/revisar_vencimientos.py
+++ proyecto/revisar_vencimientos.py
@@ -1,6 +1,24 @@
 import json 
+from  datetime import datetime, timedelta
+# Comienzo a leer prodcutos.json y los agrego en una variable
 with open ('productos.json', 'r', encoding='utf-8') as vencimientos:
     productos = json.load(vencimientos)
 
-print("productos cargados: ")
-for producto in productos :
+
+    
+hoy = datetime.today()
+
+
+
+#recorro la lista de productos y chequeo la fecha de vto comparandola con hoy
+for producto in productos:#recolecto los datos de la variable productos
+    nombre = producto['nombre']
+    sucursal = producto['sucursal']
+    fecha_str = producto['fecha_vencimiento']
+    #convierto la fecha a un objeto de datetime(desde str a formato dias)
+    fecha_venc = datetime.strptime(fecha_str,'%Y-%m-%d')
+    dias_restante = (fecha_venc - hoy).days
+    #chekeo si se cumple la fecha de vto o no 
+    if dias_restante<= 15: 
+        print(f'ALERTA {nombre} | Vence en  {dias_restante}  | en {sucursal}')
+    
